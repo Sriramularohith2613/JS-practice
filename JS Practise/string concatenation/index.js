5c0ad97d3fe3e484959f682ba785b03d3edf5362
Sriramularohith2613 <131951112+Sriramularohith2613@users.noreply.github.com>
2023-05-15 08:05:19 +0530

    Add files via upload

---
+++ JS Practise/string concatenation/index.js
@@ -0,0 +1,39 @@
+//  + operator 
+
+var txt1 = "What is";
+var txt2 = "your name?";
+// document.getElementById("demo").innerHTML = txt1 + txt2;
+
+
+//  space in string
+
+var firstName = "Sriramula ";
+var lastName = "Rohith";
+var fullName = firstName + lastName;
+// document.write(fullName)
+
+// space in btw them
+
+var firstName1 = "Ram";
+var secondName1 = "Charan";
+var lastName1 = "Tej";
+
+var fullName1 = firstName1 + " " + secondName1 + " " + lastName1;
+// document.write(fullName1)
+
+
+
+// += operator 
+
+var firstName2 = "virat";
+firstName2 += " Kohli";
+// document.write(firstName2)
+
+
+// Adding strings and num
+let x = 5 + 10;
+let y = "100" + 4;
+let z = 4 + 4 + "10" + 5 + 5;
+let w = "hello " + 6;
+
+// document.write(w)
